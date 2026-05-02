5865b667ac793fc6de635bcf4e792feeaa42f254
IlyaVafin <rthrthrer5gh@gmail.com>
2026-05-02 23:01:11 +0300

    separated header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { $fetch } from "../api/api"
+import Header from "../components/Header"
 
 const Home = () => {
 	const [videos, setVideos] = useState()
@@ -26,65 +27,7 @@ const Home = () => {
 
 	return (
 		<>
-			<nav className='navbar navbar-expand-lg fixed-top'>
-				<div className='container'>
-					<div
-						className='align-items-center w-100 d-flex justify-content-between gap-xxl-4'
-						id='navbarNav'
-					>
-						<a href='index.html' className='sidebar-brand'>
-							<h2>
-								R<span className='brand-accent'>Tub</span>
-							</h2>
-						</a>
-
-						<div className='d-flex align-items-center' id='auth-buttons'>
-							<a href='login.html' className='btn btn-outline-primary me-2'>
-								Войти
-							</a>
-							<a href='register.html' className='btn btn-primary'>
-								Регистрация
-							</a>
-						</div>
-
-						<div
-							className='d-flex align-items-center'
-							id='user-menu'
-							style={{ display: "none !important" }}
-						>
-							<div className='dropdown'>
-								<button
-									className='btn btn-outline-primary dropdown-toggle'
-									type='button'
-									data-bs-toggle='dropdown'
-								>
-									<i className='bi bi-person-circle me-1'></i> User123
-								</button>
-								<ul className='dropdown-menu'>
-									<li>
-										<a className='dropdown-item' href='my_channel.html'>
-											<i className='bi bi-person me-2'></i> Мой канал
-										</a>
-									</li>
-									<li>
-										<a className='dropdown-item' href='create_video.html'>
-											<i className='bi bi-upload me-2'></i> Загрузить видео
-										</a>
-									</li>
-									<li>
-										<hr className='dropdown-divider' />
-									</li>
-									<li>
-										<a className='dropdown-item text-danger' href='#'>
-											<i className='bi bi-box-arrow-right me-2'></i> Выйти
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</nav>
+			<Header />
 
 			<div id='explore-page' className='page'>
 				<div className='container py-4'>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router"
 import { $fetch } from "../api/api"
+import Header from "../components/Header"
 
 const Login = () => {
 	const [loginData, setLoginData] = useState({ email: "", password: "" })
@@ -26,65 +27,7 @@ const Login = () => {
 
 	return (
 		<>
-			<nav className='navbar navbar-expand-lg fixed-top'>
-				<div className='container'>
-					<div
-						className='align-items-center w-100 d-flex justify-content-between gap-xxl-4'
-						id='navbarNav'
-					>
-						<Link to='/' className='sidebar-brand'>
-							<h2>
-								R<span className='brand-accent'>Tub</span>
-							</h2>
-						</Link>
-
-						<div className='d-flex align-items-center' id='auth-buttons'>
-							<Link to='/login' className='btn btn-outline-primary me-2'>
-								Войти
-							</Link>
-							<Link to='/register' className='btn btn-primary'>
-								Регистрация
-							</Link>
-						</div>
-
-						<div
-							className='d-flex align-items-center'
-							id='user-menu'
-							style={{ display: "none !important" }}
-						>
-							<div className='dropdown'>
-								<button
-									className='btn btn-outline-primary dropdown-toggle'
-									type='button'
-									data-bs-toggle='dropdown'
-								>
-									<i className='bi bi-person-circle me-1'></i> User123
-								</button>
-								<ul className='dropdown-menu'>
-									<li>
-										<Link className='dropdown-item' to='/profile'>
-											<i className='bi bi-person me-2'></i> Мой канал
-										</Link>
-									</li>
-									<li>
-										<Link className='dropdown-item' href='/create-video'>
-											<i className='bi bi-upload me-2'></i> Загрузить видео
-										</Link>
-									</li>
-									<li>
-										<hr className='dropdown-divider' />
-									</li>
-									<li>
-										<a className='dropdown-item text-danger' href='#'>
-											<i className='bi bi-box-arrow-right me-2'></i> Выйти
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</nav>
+			<Header />
 
 			<div id='login-page' className='page'>
 				<div className='auth-form'>
